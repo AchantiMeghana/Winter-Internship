ab319122e472d72efb0c52837cb26f185c5e65a6
AchantiMeghana <121286942+AchantiMeghana@users.noreply.github.com>
2022-12-31 11:36:00 +0530

    Update app_Linear_Regression.R

--- code/Bivariate Linear Regression/app_Linear_Regression.R
+++ code/Bivariate Linear Regression/app_Linear_Regression.R
@@ -17,15 +17,15 @@ ui <- fluidPage(
            fileInput("upload","Upload your dataset",multiple= FALSE,
                      accept = c(".csv",".tsv")),
                       
-# Select independent variable x           
+# Widget to select independent variable x           
            selectInput("x","Choose the independent variable",
                        choices = NULL),
 
-# Select independent variable y          
+# Widget to choose independent variable y          
            selectInput("y","Choose the dependent variable",
                        choices = NULL),
            
-#Choose no. of rows of table to be displayed
+#Choose no. of rows of dataset to be displayed
            sliderInput("n","Choose the number of rows of data to be displayed",
                        min= 0, max =0 ,step = 1,value =0)
     ),
@@ -44,16 +44,16 @@ ui <- fluidPage(
 #display columns of dataset
   fluidRow(
     column(9,
+        #Display dataset
         tableOutput("table")   
       )),
-
-
 )   
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output,session) {
  
-#storing dataset in reactive variable
+#Storing dataset in reactive variable
   data <- reactive({
     req(input$upload,cancelOutput = TRUE) #program doesn't run if condition is false
     ext <- tools::file_ext(input$upload$name)
@@ -62,7 +62,7 @@ server <- function(input, output,session) {
             csv  = vroom::vroom(input$upload$datapath,delim =","),
             validate("Upload a .csv or .tsv file"))}) 
 
- #Update slider range
+  #Update widget range od no. of rows to be displayed
     observeEvent(data(),{
       updateSliderInput(inputId = "n",max = nrow(data()))
     }) 
@@ -72,42 +72,30 @@ server <- function(input, output,session) {
     output$table <- renderTable(head(data(),input$n))
 })
   
-  #Update choices in widgets for x,y
+  #Update choices in widgets for x
   observeEvent(data(),{
     updateSelectInput(inputId = "x",choices = colnames(data()))
   })
+  
+  #Update choices in widgets for y
   observeEvent(data(),{
     updateSelectInput(inputId = "y",choices = colnames(data()))
   })
   
-#   #Plotting the linear regression graph
-#   output$regression_plot <- renderPlot({
-#     plot(as.numeric(unlist(data()[,input$x]))
-#          ,as.numeric(unlist(data()[,input$y]))
-#          ,xlab = input$x, ylab = input$y,col = "darkred")
-#     abline(lm(as.numeric(unlist(data()[,input$y]))
-#               ~as.numeric(unlist(data()[,input$x]))))
-#   })
-# 
-#   # Store summary of regression
-# result <- reactive({
-#   req(input$upload)
-#   lmdata <- lm(as.numeric(unlist(data()[,input$y]))
-#                 ~as.numeric(unlist(data()[,input$x])))
-#   summary(lmdata)
-# })
-
-  # x
+  # Store X values 
   x<- reactive(as.numeric(unlist(data()[,input$x])))
+  
+  #Store Y values
   y <- reactive(as.numeric(unlist(data()[,input$y])))
 
   
- #Linear regression graph
+ #Linear regression model
   model <- reactive({
     req(input$upload)
     lm(y()~x(),data())
   })
   
+  #Plot linear regression graph
   output$regression_plot <- renderPlot({
     ggplot(data(),
            aes(x(),y()))+ geom_point(alpha = 0.8)+ 
@@ -117,17 +105,14 @@ server <- function(input, output,session) {
 
   })
  
-#Display the result
-output$display_result <- renderPrint(result())
-
 #Downloading the report
 output$download <- downloadHandler(
    filename = "report.html",
-  content = function(file) {
+  content = function(file) { #content of file
    
     #Using a temporary directory
     tempReport <- file.path(tempdir(), "report.Rmd")
-    file.copy("report.Rmd", tempReport, overwrite = TRUE)
+    file.copy("report_Bivariate_Linear_Regression.Rmd", tempReport, overwrite = TRUE)
     
     # Setting up parameters to pass to Rmd document
     params <- model()
@@ -137,9 +122,6 @@ output$download <- downloadHandler(
                       params = params)}
 )
   
-
-  
-
 }
 
 # Run the application 
